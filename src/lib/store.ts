d2cbe49ba100412a83973ca38c9cd85603c48706
LongPahm2003 <longp357925@gmail.com>
2024-10-16 22:00:25 +0700

    Fecth API user ADmin + Delete user by id

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../lib/features/auth/authSlice';
 import petReducer from "../lib/features/pet/petSlice"
 import healthCheckReducer  from "../lib/features/pet/HealthCheckSlice"
+import userReducer from "../lib/features/user/userSlice"
 export const makeStore = () => {
   return configureStore({
     reducer: {
         auth: authReducer,
          pets: petReducer,
          healthChecks: healthCheckReducer,
+         user: userReducer, 
     }
   })
 }
